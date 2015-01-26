ec2dc198e561d91d44bb0f669fb233f80959f91d
bulejava <bulejava@126.com>
2015-01-27 01:02:18 +0800

    调整目录结构
    调整目录结构

---
+++ src/main/java/netty/network/chat/server/ChatServerInitializer.java
@@ -0,0 +1,26 @@
+package netty.network.chat.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class ChatServerInitializer extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	protected void initChannel(SocketChannel ch) throws Exception {
+		
+		ChannelPipeline pipline=ch.pipeline();
+		 pipline.addLast("framer",new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()))
+		 .addLast("encoder",new StringEncoder())
+		 .addLast("decoder",new StringDecoder())
+		 .addLast("handler",new ChatServerHandler());
+		
+		
+		
+	}
+
+}
